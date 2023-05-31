4f4894ad74de7741cd219f1159c2267dae4b0257
Night-stars-1 <99261160+Night-stars-1@users.noreply.github.com>
2023-05-31 18:19:46 +0800

    Add files via upload

---
+++ 星穹铁道小助手/星穹铁道小助手.cpp
@@ -0,0 +1,17 @@
+﻿// 星穹铁道小助手.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <windows.h>
+using namespace std;
+
+void HideWindow() {
+	HWND hwnd = GetForegroundWindow();
+	if (hwnd) {
+		ShowWindow(hwnd, SW_HIDE);
+	}
+}
+
+int main() {
+	HideWindow();
+	system("python gui.py");
+}
